1547934cbeefdf3243363c7deafcac9f86b066d2
mahadi40011 <mahadi40011@gmail.com>
2026-01-08 08:27:56 +0600

    Install nanoid. Create a post API for generate shortCode and date save into database

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb')
-const admin = require('firebase-admin')
-const port = process.env.PORT || 3000
-const decoded = Buffer.from(process.env.FB_SERVICE_KEY, 'base64').toString(
-  'utf-8'
-)
-const serviceAccount = JSON.parse(decoded)
+require("dotenv").config();
+const express = require("express");
+const { customAlphabet } = require("nanoid");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const admin = require("firebase-admin");
+const port = process.env.PORT || 3000;
+const decoded = Buffer.from(process.env.FB_SERVICE_KEY, "base64").toString(
+  "utf-8"
+);
+const serviceAccount = JSON.parse(decoded);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-})
+});
 
-const app = express()
+const app = express();
 // middleware
 app.use(
   cors({
@@ -21,23 +22,23 @@ app.use(
     optionSuccessStatus: 200,
   })
 );
-app.use(express.json())
+app.use(express.json());
 
 // jwt middlewares
 const verifyJWT = async (req, res, next) => {
-  const token = req?.headers?.authorization?.split(' ')[1]
-  console.log(token)
-  if (!token) return res.status(401).send({ message: 'Unauthorized Access!' })
+  const token = req?.headers?.authorization?.split(" ")[1];
+  // console.log(token);
+  if (!token) return res.status(401).send({ message: "Unauthorized Access!" });
   try {
-    const decoded = await admin.auth().verifyIdToken(token)
-    req.tokenEmail = decoded.email
-    console.log(decoded)
-    next()
+    const decoded = await admin.auth().verifyIdToken(token);
+    req.tokenEmail = decoded.email;
+    // console.log({decoded});
+    next();
   } catch (err) {
-    console.log(err)
-    return res.status(401).send({ message: 'Unauthorized Access!', err })
+    console.log(err);
+    return res.status(401).send({ message: "Unauthorized Access!", err });
   }
-}
+};
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(process.env.MONGODB_URI, {
@@ -46,24 +47,72 @@ const client = new MongoClient(process.env.MONGODB_URI, {
     strict: true,
     deprecationErrors: true,
   },
-})
+});
 async function run() {
   try {
+    const db = client.db("URl-Shortener");
+    const urlsCollection = db.collection("URLs");
+
+    //generate shortCode and date save into database
+    app.post("/generate-shortCode", verifyJWT, async (req, res) => {
+      const email = req.tokenEmail;
+      const { longUrl } = req.body;
+      const alphabet = "abcdefghijklmnopqrstuvwxyz0123456789";
+      const generateShortCode = customAlphabet(alphabet, 8);
+
+      if (!longUrl) {
+        return res.status(400).send({ message: "Long URL is required" });
+      }
+
+      try {
+        const existingEntry = await urlsCollection.findOne({
+          longUrl: longUrl,
+        });
+
+        if (existingEntry) {
+          return res.status(200).send({
+            shortCode: existingEntry.shortCode,
+            message: "Short URL generated successfully",
+          });
+        }
+
+        const shortCode = generateShortCode();
+
+        const newUrlEntry = {
+          email,
+          longUrl,
+          shortCode,
+          createdAt: new Date(),
+          totalVisit: 0,
+        };
+
+        const result = await urlsCollection.insertOne(newUrlEntry);
+
+        res.status(201).send({
+          shortCode: shortCode,
+          message: "Short URL generated successfully",
+        });
+      } catch (error) {
+        console.error("Generation Error:", error);
+        res.status(500).send({ message: "Internal server error" });
+      }
+    });
+
     // Send a ping to confirm a successful connection
-    await client.db('admin').command({ ping: 1 })
+    await client.db("admin").command({ ping: 1 });
     console.log(
-      'Pinged your deployment. You successfully connected to MongoDB!'
-    )
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
   }
 }
-run().catch(console.dir)
+run().catch(console.dir);
 
-app.get('/', (req, res) => {
-  res.send('Hello from Server..')
-})
+app.get("/", (req, res) => {
+  res.send("Hello from Server..");
+});
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`)
-})
+  console.log(`Server is running on port ${port}`);
+});
